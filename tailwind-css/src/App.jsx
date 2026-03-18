ac35ceb2eb486b357f2367bc39f0a8083092690c
Md Raza <mdraza8397@gmail.com>
2026-03-18 21:51:16 +0530

    tailwind integrate and use also

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+      <h1 className='bg-red-600 text-white p-2'>Md Raza</h1>
+    </div>
+  )
+}
+
+export default App
