71fb443db340bf795ea9b0a6b4f7783b984cf8d8
Md Raza <mdraza8397@gmail.com>
2026-04-01 15:16:34 +0530

    two way binding done

---
+++ two-way-binding/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const formHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form
+        action=""
+        onSubmit={(e) => {
+          formHandler(e);
+          console.log("Submitted");
+        }}
+      >
+        {/* <h1>{name}</h1> */}
+        <input
+          className="px-3 py-2 border rounded-md shadow-sm"
+          type="text"
+          placeholder="Enter Your Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+        />
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
