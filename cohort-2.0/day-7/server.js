937a4dfbc3cd024edc6bf0f08a5109a7149b1903
Md Raza <mdraza8397@gmail.com>
2026-05-16 21:17:00 +0530

    .env file added

--- cohort-2.0/day-7/server.js
+++ cohort-2.0/day-7/server.js
@@ -1,6 +1,7 @@
 const app = require("./src/app");
 const mongoose = require("mongoose");
 const connectToDb = require("./src/config/database");
+require("dotenv").config();
 
 connectToDb();
 
--- cohort-2.0/day-7/src/config/database.js
+++ cohort-2.0/day-7/src/config/database.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
 function connectToDb() {
-  mongoose
-    .connect(
-      "mongodb+srv://mdraza8297_db_user:tAtlY8kJV9JpzM8p@cluster0.kcbidcv.mongodb.net/day-7",
-    )
-    .then(() => {
-      console.log("Connected to Database");
-    });
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to Database");
+  });
 }
 
 module.exports = connectToDb;
