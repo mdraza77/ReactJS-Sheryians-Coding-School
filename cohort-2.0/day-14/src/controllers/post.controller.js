05b7a09c5c2000abc8899f354125472a55cb6d15
Md Raza <mdraza8397@gmail.com>
2026-05-21 01:29:15 +0530

    pos creation api done

--- cohort-2.0/day-14/src/controllers/post.controller.js
+++ cohort-2.0/day-14/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const imageKit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -9,13 +10,38 @@ const imageKit = new ImageKit({
 const createPostController = async (req, res) => {
   const file = await imageKit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
-    fileName: "test",
+    fileName: "post",
+    folder: "insta-clone",
   });
 
-  //   res.send(file);
-  res.status(200).json({
-    message: "File uploaded to ImageKit",
-    file: file,
+  const jwt_token = req.cookies.jwt_token;
+
+  if (!jwt_token) {
+    return res.status(401).json({
+      message: "Access denied, Token not provided",
+    });
+  }
+
+  let verify_jwt_token = null;
+
+  try {
+    verify_jwt_token = jwt.verify(jwt_token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized access",
+      error: error.message,
+    });
+  }
+
+  const post = await postModel.create({
+    caption: req.body.caption,
+    image_path: file.url,
+    user: verify_jwt_token.id,
+  });
+
+  res.status(201).json({
+    message: "Post created successfully",
+    post: post,
   });
 };
 
